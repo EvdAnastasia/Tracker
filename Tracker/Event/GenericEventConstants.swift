17f64b50dd116321b8e916e2a67ee7870c56c3ea
EvdAnastasia <anastasia.evdokimovitch@yandex.ru>
2025-01-17 18:56:18 +0300

    Рефакторинг

--- Tracker/Event/GenericEventConstants.swift
+++ Tracker/Event/GenericEventConstants.swift
@@ -26,5 +26,24 @@ enum GenericEventConstants {
     static let styleHeaderReuseIdentifier: String = "StyleHeaderSection"
     
     static let emoji: [String] = ["🙂","😻","🌺","🐶","❤️","😱","😇","😡","🥶","🤔","🙌","🍔","🥦","🏓","🥇","🎸","🏝","😪"]
-    static let colors: [UIColor] = [.ypColorSelection1, .ypColorSelection2, .ypColorSelection3, .ypColorSelection4, .ypColorSelection5, .ypColorSelection6, .ypColorSelection7, .ypColorSelection8, .ypColorSelection9, .ypColorSelection10, .ypColorSelection11, .ypColorSelection12, .ypColorSelection13, .ypColorSelection14, .ypColorSelection15, .ypColorSelection16, .ypColorSelection17, .ypColorSelection18]
+    static let colors: [UIColor] = [
+        .ypColorSelection1,
+        .ypColorSelection2,
+        .ypColorSelection3,
+        .ypColorSelection4,
+        .ypColorSelection5,
+        .ypColorSelection6,
+        .ypColorSelection7,
+        .ypColorSelection8,
+        .ypColorSelection9,
+        .ypColorSelection10,
+        .ypColorSelection11,
+        .ypColorSelection12,
+        .ypColorSelection13,
+        .ypColorSelection14,
+        .ypColorSelection15,
+        .ypColorSelection16,
+        .ypColorSelection17,
+        .ypColorSelection18
+    ]
 }
