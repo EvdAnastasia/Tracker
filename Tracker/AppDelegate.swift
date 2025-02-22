7b856fa526d222410296b79662baa50dcc58f6ed
EvdAnastasia <anastasia.evdokimovitch@yandex.ru>
2025-02-22 18:04:29 +0300

    Добавлена AppMetrica + базовая конфигурация

--- Tracker/AppDelegate.swift
+++ Tracker/AppDelegate.swift
@@ -6,15 +6,20 @@
 //
 
 import UIKit
+import AppMetricaCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-    
+
     func application(
         _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool { true }
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?
+    ) -> Bool {
+        let configuration = AppMetricaConfiguration(apiKey: "6343105b-7b89-43c9-beda-8fa680d046c8")
+        AppMetrica.activate(with: configuration!)
+        return true
+    }
     
     // MARK: UISceneSession Lifecycle
     func application(
